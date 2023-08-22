9b2bb51b5fff4ce08e82b03251447784c65ab7a5
VennV <111500380+VennDev@users.noreply.github.com>
2023-08-22 19:21:00 +0700

    Add files via upload

---
+++ src/com/superpig1953/server/Server.java
@@ -0,0 +1,32 @@
+package com.superpig1953.server;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    private static final String IP = "127.0.0.1";
+
+    private static final int PORT = 25565;
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(IP))) {
+            System.out.println("Server started: " + serverSocket);
+
+            try (Socket socket = serverSocket.accept()) {
+                System.out.println("Client connected: " + socket.getInetAddress().getHostAddress());
+
+                try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter output = new PrintWriter(socket.getOutputStream(), true)) {
+
+                    String message;
+                    while ((message = input.readLine()) != null) {
+                        System.out.println("Client message: " + message);
+                        output.println("Server received the message [" + message + "]");
+                    }
+                }
+            }
+        }
+    }
+
+}
